c5fb7b7d751a2c0606a5d3f5f99fb08508a6c073
sunlinlin <sunlinlin@aragoncs.com>
2017-09-14 15:19:40 +0800

    Revert "first: commit"
    This reverts commit faeafc1e537acffb91730a2514f788acc075ed9d.

--- weibo/weibos/apps/sina/tests.py
+++
@@ -1,6 +0,0 @@
-#
-#
-# {"access_token":"2.00nRGwPC0i6l_j4cba3099e5Vh7z_C","remind_in":"157679999","expires_in":157679999,"uid":"2067757895"}
-#
-#
-# curl --data 'title=hello&content=world&cover=//image.brilliantearth.com/media/cache/07/5b/075b6cbffbcd558a8d7ceacc1c8409a5.jpg&summary=python&textjava&access_token=2.00nRGwPC0i6l_j4cba3099e5Vh7z_C' https://api.weibo.com/proxy/article/publish.json
--- weibo/weibos/url_settings.py
+++
@@ -1,2 +0,0 @@
-# article api
-ARTICLE = 'https://api.weibo.com/proxy/article/publish.json'
