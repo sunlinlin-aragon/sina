afa4dc6c64021c2f217225025b07945307d846ae
sunlinlin <sunlinlin@aragoncs.com>
2017-11-05 19:44:58 +0800

    FIX: # fix static file

--- sinaweibo/weibos/django_settings.py
+++ sinaweibo/weibos/django_settings.py
@@ -110,11 +110,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, 'weibos/media/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'weibos/static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'weibos/static/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'django_static')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "weibos/static"),
+]
 
 
 CRONJOBS = [
