5569da2b578c0edd39badc0159fe2c8f9f19a47f
sunlinlin <sunlinlin@aragoncs.com>
2018-01-01 12:10:54 +0800

    FIX: ok

--- sinaweibo/weibos/apps/sina/crontab.py
+++ sinaweibo/weibos/apps/sina/crontab.py
@@ -6,11 +6,14 @@ from django.conf import settings
 
 
 def send_sina_weibo(*args, **kwargs):
-    sina_token = SinaApiData.objects.first().token
-    send_articles = Article.objects.filter(send_datetime__gte = datetime.datetime.now() + datetime.timedelta(hours=-1), is_send = False)
-    for article in send_articles:
-        params = article.get_params()
-        params['access_token'] = sina_token
-        response = requests.post(settings.ARTICLE, data=params)
-        print response
-    print 'send success'
+    try:
+        sina_token = SinaApiData.objects.first().token
+        send_articles = Article.objects.filter(send_datetime__gte = datetime.datetime.now() + datetime.timedelta(hours=-1), is_send = False)
+        for article in send_articles:
+            params = article.get_params()
+            params['access_token'] = sina_token
+            response = requests.post(settings.ARTICLE, data=params)
+            print response
+        print 'send success'
+    except Exception, e:
+        print str(e)
