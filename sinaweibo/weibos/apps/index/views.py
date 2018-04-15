411450ab61afe163c94acbc3f977d3c8b2f0723f
sll <sunll03@missfresh.cn>
2018-04-15 17:22:04 +0800

    FIX: #

--- sinaweibo/weibos/apps/index/views.py
+++ sinaweibo/weibos/apps/index/views.py
@@ -2,13 +2,16 @@ from django.template.response import TemplateResponse
 from django.http import JsonResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from weibos.apps.sina.models import Article, Banner, Category, Questions, ExaminationPointCategory
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/admin/login/')
 def home_page(request, id=1):
     if request.is_ajax():
         id = request.GET.get('id') or 1
     questions = Questions.objects.filter(category_id=id).order_by('-look_num').values('id', 'title', 'look_num')
     paginator = Paginator(questions, 10)
+    category = Category.objects.get(id=id)
     if request.is_ajax():
         page = request.GET.get('page')
         json_context = paginator.page(page)
@@ -28,10 +31,12 @@ def home_page(request, id=1):
             'page_number': 1,
             'page_range': page_range,
             'meta_title': meta_title.title if meta_title else '',
+            'category': category.title
         }
         return TemplateResponse(request, template, context)
 
 
+@login_required(login_url='/admin/login/')
 def examination_list_page(request, id):
     template = 'examination_page.html'
     category_list = Category.objects.all()
@@ -46,6 +51,7 @@ def examination_list_page(request, id):
     return TemplateResponse(request, template, context)
 
 
+@login_required(login_url='/admin/login/')
 def list_page(request, id=1):
     template = 'list.html'
     if request.is_ajax():
@@ -72,6 +78,7 @@ def list_page(request, id=1):
         return TemplateResponse(request, template, context)
 
 
+@login_required(login_url='/admin/login/')
 def question_page(request, id):
     template = 'question_info.html'
     question_info = Questions.objects.filter(id=id).first()
